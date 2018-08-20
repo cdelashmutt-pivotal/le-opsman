3e019d325b70242245b644ad46f094db190de9f5
Chris DeLashmutt <cdelashmutt@pivotal.io>
2018-08-20 16:38:43 -0400

    Adding compression and a pipeline

--- tasks/le-retrieve.sh
+++ tasks/le-retrieve.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
 /usr/local/bin/acme.sh --issue --dns dns_gd -d grogscave.net -d opsman.cf.grogscave.net --cert-file ./crypto-materials/cert.pem --key-file ./crypto-materials/key.pem --fullchain-file ./crypto-materials/fullchain.pem
+tar czvf ./crypto-materials/crypto.tgz ./crypto-materials/cert.pem ./crypto-materials/key.pem ./crypto-materials/fullchain.pem
