de03bc744a0509ff2a981b8fd04d761f2211fbae
Chris DeLashmutt <cdelashmutt@pivotal.io>
2018-08-21 11:21:34 -0400

    Adding upload of cert

--- tasks/le-retrieve.sh
+++ tasks/le-retrieve.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-/usr/local/bin/acme.sh --issue --dns dns_gd -d grogscave.net -d opsman.cf.grogscave.net --cert-file ./crypto-material/cert.pem --key-file ./crypto-material/key.pem --fullchain-file ./crypto-material/fullchain.pem
-tar czvf ./crypto-material/crypto-$(cat ./input-version/version).tgz ./crypto-material/cert.pem ./crypto-material/key.pem ./crypto-material/fullchain.pem
+cd ./crypto-material
+
+/usr/local/bin/acme.sh --issue --dns dns_gd -d grogscave.net -d opsman.cf.grogscave.net --cert-file cert.pem --key-file key.pem --fullchain-file fullchain.pem
+tar czvf crypto-$(cat ./input-version/version).tgz cert.pem key.pem fullchain.pem
