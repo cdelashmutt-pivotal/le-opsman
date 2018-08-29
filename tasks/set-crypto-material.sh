b269cb0124c23f43dd407dae533e09343cb39ba9
Chris DeLashmutt <cdelashmutt@pivotal.io>
2018-08-29 15:50:57 -0400

    Set executable bit

--- tasks/set-crypto-material.sh
+++ tasks/set-crypto-material.sh
@@ -3,12 +3,11 @@ set -e
 
 cd ./crypto-material
 
-CERT=$(cat fullchain.pem)
-KEY=$(cat key.pem)
- 
+CERT="$(sed -e 's/$/\\r\\n/g' < fullchain.pem | tr -d '\n')"
+KEY="$(sed -e 's/$/\\r\\n/g' < key.pem | tr -d '\n')"
+
 uaac target https://$OPSMAN_HOST/uaa --skip-ssl-validation
 uaac token owner get opsman $OPSMAN_USER -s "" -p $OPSMAN_PASSWORD
 ACCESS_TOKEN=$( uaac context | grep access_token | tr -s " " | cut -d " " -f 3 )
 
-curl "https://$OPSMAN_HOST/v0/settings/ssl_certificate" -X PUT -H "Authorization: Bearer $ACCESS_TOKEN" \
-  -d "{\"ssl_certificate\": { \"certificate\": \"$CERT\", \"private_key\": \"$KEY\" } }"
+curl "https://$OPSMAN_HOST/api/v0/settings/ssl_certificate" -X PUT -H "Authorization: Bearer $ACCESS_TOKEN" -H "Content-Type: application/json" -d "{\"ssl_certificate\": { \"certificate\": \"$CERT\", \"private_key\": \"$KEY\" } }" -v
