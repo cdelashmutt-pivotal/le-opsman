f7b529b944585164c9d9ec1e5b9dfa1b867f9132
Chris DeLashmutt <grog@grogscave.net>
2019-03-26 18:21:47 -0400

    Allow bad ssl

--- tasks/set-crypto-material.sh
+++ tasks/set-crypto-material.sh
@@ -10,4 +10,4 @@ uaac target https://$OPSMAN_HOST/uaa --skip-ssl-validation
 uaac token owner get opsman $OPSMAN_USER -s "" -p $OPSMAN_PASSWORD
 ACCESS_TOKEN=$( uaac context | grep access_token | tr -s " " | cut -d " " -f 3 )
 
-curl "https://$OPSMAN_HOST/api/v0/settings/ssl_certificate" -X PUT -H "Authorization: Bearer $ACCESS_TOKEN" -H "Content-Type: application/json" -d "{\"ssl_certificate\": { \"certificate\": \"$CERT\", \"private_key\": \"$KEY\" } }" -v
+curl "https://$OPSMAN_HOST/api/v0/settings/ssl_certificate" -X PUT -H "Authorization: Bearer $ACCESS_TOKEN" -H "Content-Type: application/json" -d "{\"ssl_certificate\": { \"certificate\": \"$CERT\", \"private_key\": \"$KEY\" } }" -v -k
