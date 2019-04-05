91b52af38e99878b8734430c9be823e510e54b6c
Chris DeLashmutt <cdelashmutt@pivotal.io>
2019-04-04 22:21:47 -0400

    change to credhub

--- tasks/le-retrieve-cf.sh
+++ tasks/le-retrieve-cf.sh
@@ -2,4 +2,4 @@
 
 cd ./crypto-material
 
-/usr/local/bin/acme.sh --issue --dns dns_gd -d cf.grogscave.net -d *.cf.grogscave.net -d *.uaa.cf.grogscave.net -d *.login.cf.grogscave.net --cert-file cert.pem --key-file key.pem --fullchain-file fullchain.pem
+/usr/local/bin/acme.sh --issue --dns dns_gd -d cf.grogscave.net -d *.cf.grogscave.net -d *.uaa.cf.grogscave.net -d *.login.cf.grogscave.net --cert-file cert.pem --key-file key.pem --ca-file ca.pem
--- tasks/set-crypto-material-cf.sh
+++ tasks/set-crypto-material-cf.sh
@@ -3,18 +3,4 @@ set -e
 
 cd ./crypto-material
 
-CERT="$(sed -e 's/$/\\r\\n/g' < fullchain.pem | tr -d '\n')"
-KEY="$(sed -e 's/$/\\r\\n/g' < key.pem | tr -d '\n')"
-
-cat << EOF > cert.cfg
-product-name: cf
-product-properties:
-  .properties.networking_poe_ssl_certs:
-    value:
-    - name: default
-      certificate:
-        cert_pem: "$CERT"
-        private_key_pem: "$KEY"
-EOF
-
-om -t $OPSMAN_HOST -u $OPSMAN_USER -p $OPSMAN_PASSWORD configure-product -c cert.cfg
+credhub set -t certificate -n /concourse/main/properties_networking_poe_ssl_certs_0_certificate --root ca.pem --certificate cert.pem --private key.pem
