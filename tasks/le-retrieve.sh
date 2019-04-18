4df87467099d15f3634ede8380d13c58c3f56b10
Chris DeLashmutt <cdelashmutt@pivotal.io>
2019-04-18 08:24:46 -0400

    Reorder parms

--- tasks/le-retrieve.sh
+++ tasks/le-retrieve.sh
@@ -8,4 +8,4 @@ do
   hostparam="$hostparam -d $h"
 done
 
-/usr/local/bin/acme.sh --issue --dns dns_gd $hostparam --cert-file cert.pem --key-file key.pem --fullchain-file fullchain.pem --ca-file ca.pem "%@"
+/usr/local/bin/acme.sh --issue --dns dns_gd $hostparam --key-file key.pem --fullchain-file fullchain.pem --cert-file cert.pem --ca-file ca.pem "%@"
